d10969890aaab83b09a5d1b5cc34b0c458ed5c6a
mfoster58 <77993506+mfoster58@users.noreply.github.com>
2022-03-16 18:28:44 -0500

    Delete database.php

--- database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$client = new MongoDB\Client(
-      'mongodb+srv://qinshu1013:J9s9eSSAmqm0c249@cse330final.mxgnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-?>
