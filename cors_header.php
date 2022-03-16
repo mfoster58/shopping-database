7c889d68a1115c38344679073832bf995585243c
mfoster58 <77993506+mfoster58@users.noreply.github.com>
2022-03-16 18:28:37 -0500

    Delete cors_header.php

--- cors_header.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ref:https://exerror.com/response-to-preflight-request-doesnt-pass-access-control-check-no-access-control-allow-origin-header-is-present-on-the-requested-resource/#Solution_1_Turn_off_CORS
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-//end of citation
-?>
